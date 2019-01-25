53922d1e5d0344f27119352f35ba7ce2c1438658
Aguenier Guillaume <guillaume.aguenier@etu.univ-orleans.fr>
2019-01-25 12:04:40 +0100

    12.04 tp1

--- tp/src/main/java/univ/orleans/fr/tp1/controllers/Controller.java
+++ tp/src/main/java/univ/orleans/fr/tp1/controllers/Controller.java
@@ -1,4 +1,6 @@
 package univ.orleans.fr.tp1.controllers;
+import exceptions.MaxNbCoupsException;
+import exceptions.MotInexistantException;
 import exceptions.PseudoDejaPrisException;
 import exceptions.PseudoNonConnecteException;
 import facade.FacadeMotus;
@@ -13,39 +15,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 
 @RestController
 @RequestMapping
 public class Controller {
-    private  static FacadeMotus facadeMotus = new FacadeMotusStatic();
+    private static FacadeMotus facadeMotus = new FacadeMotusStatic();
 
-    @RequestMapping(value="/joueur",method= RequestMethod.POST,produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<String> connexion (@RequestParam String pseudo){
-        try{
+    @RequestMapping(value = "/joueur", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> connexion(@RequestParam String pseudo) {
+        try {
             facadeMotus.connexion(pseudo);
-            return ResponseEntity.created(new URI("/motus/partie"+pseudo)).build();
-        }catch (PseudoDejaPrisException | URISyntaxException e){
+            return ResponseEntity.created(new URI("/motus/partie" + pseudo)).build();
+        } catch (PseudoDejaPrisException | URISyntaxException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Pseudo deja pris");
         }
     }
 
-    @RequestMapping(value="/deco",method= RequestMethod.DELETE,produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<String> deconnexion (@RequestParam String pseudo){
-        try{
+    @RequestMapping(value = "/deco", method = RequestMethod.DELETE, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> deconnexion(@RequestParam String pseudo) {
+        try {
             facadeMotus.deconnexion(pseudo);
             return ResponseEntity.status(HttpStatus.OK).body("Bye");
-        }catch (PseudoNonConnecteException e){
+        } catch (PseudoNonConnecteException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Pseudo non connecté");
         }
     }
 
-    @RequestMapping(value="/mot",method= RequestMethod.POST,produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<String> jouer (@RequestParam String pseudo, @RequestParam String mot){
-        try{
-            facadeMotus.deconnexion(pseudo);
-            return ResponseEntity.status(HttpStatus.OK).body("Bye");
-        }catch (PseudoNonConnecteException e){
+    @RequestMapping(value = "/mot", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> jouer(@RequestParam String pseudo, @RequestParam String mot) {
+        try {
+            String res=facadeMotus.jouer(pseudo, mot);
+            return ResponseEntity.status(HttpStatus.OK).body(res);
+        } catch (PseudoNonConnecteException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Pseudo non connecté");
+        } catch (MaxNbCoupsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("MaxNbCoup");
+        } catch (MotInexistantException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Mot inexistant");
         }
     }
+
+    @RequestMapping(value = "/nouvellePartie", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> nouvellePartie(@RequestParam String pseudo,@RequestParam String dico) {
+        try {
+            facadeMotus.nouvellePartie(pseudo,dico);
+            return ResponseEntity.created(new URI("/motus/partie" + pseudo)).build();
+        } catch (PseudoNonConnecteException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Pseudo non connecté");
+        } catch (URISyntaxException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Mauvaise URI");
+        }
+    }
+
+    @RequestMapping(value = "/dicos", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> getDicos(@RequestParam String pseudo,@RequestParam String dico) {
+        Collection<String> temp = facadeMotus.getListeDicos();
+        String res = temp.toString();
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+
 }
